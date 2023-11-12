85a4f557bd3c953c9cdd92a8ef0814e436c05909
PIAIC-220943 <doctormalix@gmail.com>
2023-11-12 18:40:19 +0500

    new upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
 
-
+{/* Muhammad Rauf 0321-9992660.. */}
 <div className="main">
 <svg viewBox="60 60 1000 170">
     <path fill="#e7008a" fill-opacity="0.3" d="M0,224L80,192C160,160,320,96,480,85.3C640,75,800,117,960,112C1120,107,1280,53,1360,26.7L1440,0L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
